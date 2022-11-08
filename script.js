46a80aa6b8d8e18215961eac43bca6d0a31ec426
user name <user email>
2022-11-08 09:36:49 -0800

    Initial commit

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ window.addEventListener("load", function () {
   const ctx = canvas.getContext("2d");
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+
+  // Canvas settings
+  ctx.fillStyle = "green";
+  ctx.strokeStyle = "yellow";
+  ctx.lineWidth = 30;
+  ctx.lineCap = "round";
+
+  ctx.fillRect(10, 10, 100, 100);
+
+  ctx.beginPath();
+  ctx.moveTo(canvas.width / 2, canvas.height / 2);
+  ctx.lineTo(canvas.width / 2 - 300, canvas.height / 2);
+  ctx.stroke();
 });
